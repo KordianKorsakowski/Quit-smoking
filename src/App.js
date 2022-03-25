1157c0fd281e188ea494bd81495e18fbbba9e8ac
kordian3.0 <kordian3.0@iMac-kordian20.local>
2022-03-25 15:04:14 +0100

    add custom hooks use-input and add simply singIn component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import Form from "./components/form/Form";
 import Answer from "./components/answer/Answer";
-
+import SingIn from "./components/singIn/SingIn";
 
 
 function App() {
 const [userData, setUserData] = useState();
 const [isValid, setIsValid] = useState(false);
+const [singIn, setSingIn] = useState(false);
 
 const getUserDataHandler = (data) => {
     setUserData(data);
@@ -14,10 +15,14 @@ const getUserDataHandler = (data) => {
 const validHandler = (value) => {
   setIsValid(value);
 }
+const singInHandler = (login) => {
+  setSingIn(login);
+}
+
   return (
     <div>
-     
-      <Form onSendUserData={getUserDataHandler} onValid={validHandler}/>
+     {!singIn && <SingIn onSingIn={singInHandler}/>}
+     {singIn && <Form onSendUserData={getUserDataHandler} onValid={validHandler}/>}
       {isValid && <Answer data={userData}/>}
      
     </div>
---
+++ src/components/singIn/SingIn.js
@@ -0,0 +1,76 @@
+import useInput from "../hooks/use-input";
+import Button from "../UI/Button";
+import classes from "./SingIn.module.css";
+
+const SingIn = (props) => {
+    
+  const {
+    value: enteredLogin,
+    isValid: enteredLoginIsValid,
+    hasError: loginInputHasError,
+    valueChangeHandler: loginChangedHandler,
+    inputBlurHandler: loginBlurHandler,
+    reset: resetLoginInput,
+  } = useInput((value) => value.trim() !== '');
+
+  const {
+    value: enteredPasword,
+    isValid: enteredPasswordIsValid,
+    hasError: passwordInputHasError,
+    valueChangeHandler: passwordChangedHandler,
+    inputBlurHandler: passwordBlurHandler,
+    reset: resetPasswordInput,
+  } = useInput((value) => value.includes('#'));
+
+  let singInIsValid = false;
+
+  if(enteredLoginIsValid && enteredPasswordIsValid){
+    singInIsValid = true;
+  }
+
+  const singInHandler = (e) => {
+    e.preventDefault();
+    if (!singInIsValid) {
+      return;
+    }
+    props.onSingIn(singInIsValid);
+    resetLoginInput();
+    resetPasswordInput();
+  };
+
+  return (
+    <div>
+      <form className={classes.formContainer} onSubmit={singInHandler}>
+        <h3>Quite - smoking</h3>
+        <label htmlFor="login">Login</label>
+        <input
+          value={enteredLogin}
+          type="text"
+          id="login"
+          onBlur={loginBlurHandler}
+          onChange={loginChangedHandler}
+          className={`${loginInputHasError ? classes.invalid : ''}`}
+        />
+        {loginInputHasError && (<p className={classes.errorM}>Can't be empty string</p>)}
+        <label>Password</label>
+        <input
+          value={enteredPasword}
+          type="text"
+          id="login"
+          onBlur={passwordBlurHandler}
+          onChange={passwordChangedHandler}
+          className={`${passwordInputHasError ? classes.invalid : ''}`}
+        />
+        {passwordInputHasError && (<p className={classes.errorM}>Can't be empty string and must include #</p>)}
+        <Button type="submit">sing in</Button>
+        <div className={classes.info}>
+        <p>Remember this is dummy app don use real password and userName!!!</p>
+        <p> If you want log in your login cant't be empty and password must include "#"</p>
+        </div>
+      </form>
+      
+    </div>
+  );
+};
+
+export default SingIn;
